17dea9664f93944237e48d2b8a103c0ccc1a7ef2
UnLow1 <adam.jamka.1995@gmail.com>
2021-02-26 00:35:45 +0100

    Added unit tests for ImporterService

--- src/test/groovy/com/jamex/refereestaffer/service/ImporterServiceSpec.groovy
+++ src/test/groovy/com/jamex/refereestaffer/service/ImporterServiceSpec.groovy
@@ -1,9 +1,13 @@
 package com.jamex.refereestaffer.service
 
+import com.jamex.refereestaffer.model.entity.Referee
+import com.jamex.refereestaffer.model.entity.Team
 import com.jamex.refereestaffer.repository.GradeRepository
 import com.jamex.refereestaffer.repository.MatchRepository
 import com.jamex.refereestaffer.repository.RefereeRepository
 import com.jamex.refereestaffer.repository.TeamRepository
+import org.springframework.mock.web.MockMultipartFile
+import org.springframework.web.multipart.MultipartFile
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -21,7 +25,27 @@ class ImporterServiceSpec extends Specification {
         importerService = new ImporterService(teamRepository, refereeRepository, matchRepository, gradeRepository)
     }
 
+    // TODO create separate file for this test
+    // TODO add tests for exceptions
     def "should import data from file"() {
+        given:
+        def file = new File("data/import data file.csv")
+        MultipartFile multipartFile = new MockMultipartFile("file", new FileInputStream(file))
+        short numberOfQueuesToImport = 30
 
+        when:
+        importerService.importData(multipartFile, numberOfQueuesToImport)
+
+        then:
+        1 * teamRepository.saveAll(_)
+        1 * refereeRepository.saveAll(_)
+        480 * teamRepository.findByName(_) >> Optional.of(new Team())
+        240 * refereeRepository.findByFirstNameAndLastName(_, _) >> Optional.of(new Referee())
+        240 * matchRepository.save(_)
+        230 * gradeRepository.save(_)
+        2 * matchRepository.findAll() >> []
+        1 * refereeRepository.findAll() >> []
+        2 * gradeRepository.findAll() >> []
+        1 * teamRepository.findAll() >> []
     }
 }
