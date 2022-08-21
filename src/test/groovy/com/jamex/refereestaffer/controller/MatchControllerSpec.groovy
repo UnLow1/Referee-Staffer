1e6a894594a6967e8dd241cc4dc1739ad6e05493
UnLow1 <adam.jamka.1995@gmail.com>
2022-08-21 23:43:58 +0200

    Fix test

--- src/test/groovy/com/jamex/refereestaffer/controller/MatchControllerSpec.groovy
+++ src/test/groovy/com/jamex/refereestaffer/controller/MatchControllerSpec.groovy
@@ -83,13 +83,14 @@ class MatchControllerSpec extends Specification {
 
     def "should update match"() {
         given:
+        def matchId = 3899l
         def matchDto = MatchDto.builder().build()
         def updatedMatchDto = MatchDto.builder().build()
         def match = [] as Match
         def updatedMatch = [] as Match
 
         when:
-        def result = matchController.updateMatch(matchDto)
+        def result = matchController.updateMatch(matchDto, matchId)
 
         then:
         1 * matchConverter.convertFromDto(matchDto) >> match
